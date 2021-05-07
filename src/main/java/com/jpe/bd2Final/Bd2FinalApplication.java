7c19bc95ce17b1ff12593f281646dccc3f9fc335
unknown <juanefsta@gmail.com>
2021-05-06 23:21:40 -0300

    First Commit - Create Project

---
+++ src/main/java/com/jpe/bd2Final/Bd2FinalApplication.java
@@ -0,0 +1,13 @@
+package com.jpe.bd2Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bd2FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bd2FinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpe/bd2Final/Bd2FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpe.bd2Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bd2FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

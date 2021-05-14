00a6fae12a39b383a3b6b724597c2e1eb64261e0
unknown <juanefsta@gmail.com>
2021-05-14 19:12:02 -0300

    Add elastic into pom and modify dockerfile

--- src/main/java/com/jpe/bd2Final/Bd2FinalApplication.java
+++ src/main/java/com/jpe/bd2Final/Bd2FinalApplication.java
@@ -2,9 +2,32 @@ package com.jpe.bd2Final;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RestController
 public class Bd2FinalApplication {
+
+	@RequestMapping("/searchA")
+	public String searchA() {
+		return "Hello Docker World";
+	}
+
+	@RequestMapping("/searchB")
+	public String searchB() {
+		return "Hello Docker World";
+	}
+
+	@RequestMapping("/searchC")
+	public String searchC() {
+		return "Hello Docker World";
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "<h1>Hola, para realizar las busquedas ingresar en /searchA ó /searchB ó /searchC </h1>";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Bd2FinalApplication.class, args);
